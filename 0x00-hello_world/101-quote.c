9cc369af06b37043350bb793c3ece45dacb6b085
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-03-16 13:42:15 -0700

    zekoooo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,11 @@
  * Description: print a qoute using write function
  * 	ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: Alawys 1 (error).
+ * Return: 1 (error).
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
