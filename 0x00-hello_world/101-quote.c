fd0061695e5b997e58f35081098e37c48a634dcf
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-03-18 00:22:36 -0700

    T

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -11,8 +12,7 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 
-	write(1, quo, 59);
 	return (1);
 }
