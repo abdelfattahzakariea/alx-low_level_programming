cb27e534494e0c94a61426e9cde86843e1d86513
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-03-18 00:57:39 -0700

    T

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,8 +12,8 @@
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fputs("and that piece of art is useful\" - Dora Kor
+par, 2015-10-19\n", stdout);
 
-	write(1, quo, 59);
 	return (1);
 }
