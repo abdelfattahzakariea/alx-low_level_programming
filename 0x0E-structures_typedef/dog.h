7cecc55b3d9e9513714fc2c7435183b9685d3482
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-04-17 14:36:24 -0700

    zeko is here

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,18 +5,18 @@
  * @name: First member
  * @age: Second member
  * @owner: Third member
- *
  * Description: Longer description
 */
-struct dog      
+struct dog
 {
 	char *name;
 	float age;
-	char *owner; 
+	char *owner;
 };
 /**
  * dog_t - typedef for struct dog
  */
+
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -25,4 +25,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
