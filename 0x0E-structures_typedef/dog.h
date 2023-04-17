c887ad0bfcb27197ff10b260603af5fd15d10e5e
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-04-17 16:09:08 -0700

    zeko

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -7,13 +8,15 @@
  * @owner: Third member
  *
  * Description: Longer description
-*/
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - typedef for struct dog
  */
