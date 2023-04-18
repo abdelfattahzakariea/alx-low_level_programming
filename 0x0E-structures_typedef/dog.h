23622a95f3c895be63fb8756486266ce1e0563f6
Abdelfattah <abdelfattahzakariea@gmail.com>
2023-04-17 17:55:31 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,10 @@ struct dog
 	char *owner;
 };
 
+#endif
+
+#ifndef DOG_H
+#define DOG_H
 /**
  * dog_t - typedef for struct dog
  */
@@ -26,5 +30,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
